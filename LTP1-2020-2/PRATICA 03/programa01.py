381e1540ca490c6fddeb7c2635b832be7bcf5273
PedroSousaS <70581490+PedroSousaS@users.noreply.github.com>
2020-09-08 08:39:35 -0300

    Create programa01.py

---
+++ LTP1-2020-2/PRATICA 03/programa01.py
@@ -0,0 +1,12 @@
+#A função print() escreve na tela
+#Todos os comentários são ignorados pelo Python
+#tudo que tiver # na frente é um comentário
+
+print('Olá mundo')
+print('Saitama > Goku')
+
+#variável
+#imput() - le os dados do teclado
+nome = input('Digite seu nome aqui')
+print('bom dia', nome,'seu lindo')
+# , separar coisas
