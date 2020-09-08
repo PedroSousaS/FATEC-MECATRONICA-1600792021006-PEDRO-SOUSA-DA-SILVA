125fbb927913c4b5539d5cb7738b1b3dca61cfb3
PedroSousaS <70581490+PedroSousaS@users.noreply.github.com>
2020-09-08 08:56:48 -0300

    Create programa02.py

---
+++ LTP1-2020-2/PRATICA 03/programa02.py
@@ -0,0 +1,5 @@
+#Pede para o usuário dois números e realiza a soma deles
+numero1 = int(input('informe um valor:'))
+numero2 = int(input('Inforome outro valor:'))
+resultado = numero1 + numero2
+print('Resultado da soma:', resultado)
