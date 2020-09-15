46242982361beb389d6b343ac71b8002279c5b8a
PedroSousaS <70581490+PedroSousaS@users.noreply.github.com>
2020-09-15 08:08:22 -0300

    Create programa01.py

---
+++ LTP1-2020-2/Pratica 04/programa01.py
@@ -0,0 +1,10 @@
+#Pede ai usuário para digitar um valor para idade
+idade = int(input('informe sua idade'))
+
+#pergunta, no código, se a idade é maior ou igual a 18
+if idade >= 18:
+    #este código pertence ao bloco do if
+    print('você possui mais de 18 anos.')
+    print('Agora! você pode ver todas as páginas de jogos da steam.')
+    
+print('final do programa.')
